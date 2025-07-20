c0c92ecf8b8c3d9dad50676a66a1d26aed4b2ddb
Chris Busch <cgbusch@yahoo.com>
2025-07-19 23:24:55 -0500

    added the ability to sort laps

--- Lap.h
+++ Lap.h
@@ -1,7 +1,34 @@
-
-
-
+/**
+ * This is used to store recent laps
+ */
 struct Lap {
-   long int duration=0;
-   uint8_t lap=0;
+  long int duration = 0;
+  uint8_t lap = 0;
+
+  // Primary comparison: shorter duration; tie-breaker: lower lap number
+  bool operator<(const Lap& other) const {
+    return duration < other.duration || 
+          (duration == other.duration && lap < other.lap);
+  }
+
+  bool operator>(const Lap& other) const {
+    return duration > other.duration || 
+          (duration == other.duration && lap > other.lap);
+  }
+
+  bool operator<=(const Lap& other) const {
+    return !(other < *this);
+  }
+
+  bool operator>=(const Lap& other) const {
+    return !(*this < other);
+  }
+
+  bool operator==(const Lap& other) const {
+    return duration == other.duration && lap == other.lap;
+  }
+
+  bool operator!=(const Lap& other) const {
+    return !(*this == other);
+  }
 };
