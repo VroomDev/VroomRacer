f8ee755d5b414802541d6cdbd655a0052e739ba9
Chris Busch <cgbusch@yahoo.com>
2026-06-06 13:07:04 -0500

    reformat

--- Lap.h
+++ Lap.h
@@ -1,25 +1,26 @@
-/* 
- * VroomRacer by Chris Busch (c) 2024
- * There are no warranties express or implied with this code.
- * No guarantees of being fit for purpose.
- */
+/*
+   VroomRacer by Chris Busch (c) 2024
+   There are no warranties express or implied with this code.
+   No guarantees of being fit for purpose.
+*/
 
 /**
- * This is used to store recent laps
- */
+   This is used to store recent laps
+*/
 struct Lap {
   long int duration = 0;
   uint8_t lap = 0;
+ 
 
   // Primary comparison: shorter duration; tie-breaker: lower lap number
   bool operator<(const Lap& other) const {
-    return duration < other.duration || 
-          (duration == other.duration && lap < other.lap);
+    return duration < other.duration ||
+           (duration == other.duration && lap < other.lap);
   }
 
   bool operator>(const Lap& other) const {
-    return duration > other.duration || 
-          (duration == other.duration && lap > other.lap);
+    return duration > other.duration ||
+           (duration == other.duration && lap > other.lap);
   }
 
   bool operator<=(const Lap& other) const {
