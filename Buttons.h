67ac53e44574c3f12c338051b768f7b0a5bdff87
Chris Busch <cgbusch@yahoo.com>
2026-06-06 13:10:45 -0500

    changed how red flag percentage is set

--- Buttons.h
+++ Buttons.h
@@ -1,8 +1,8 @@
-/* 
- * VroomRacer by Chris Busch (c) 2024
- * There are no warranties express or implied with this code.
- * No guarantees of being fit for purpose.
- */
+/*
+   VroomRacer by Chris Busch (c) 2024
+   There are no warranties express or implied with this code.
+   No guarantees of being fit for purpose.
+*/
 
 #include <EEPROM.h>
 
@@ -19,13 +19,13 @@
 #define BUP 2
 
 uint8_t readButtons() {
-  uint8_t flags=0;
+  uint8_t flags = 0;
   if (digitalRead(BUTTON_MINUS) == LOW) {
-    if(serialOn) Serial.println("Down");
+    if (serialOn) Serial.println("Down");
     flags |= BDOWN;
   }
   if (digitalRead(BUTTON_PLUS) == LOW) {
-    if(serialOn) Serial.println("Up");
+    if (serialOn) Serial.println("Up");
     flags |= BUP;
   }
   return flags;
@@ -38,11 +38,11 @@ uint8_t v = 0; //current configuration setting
 bool requireUpToStart = false;
 
 /** returns eeprom mem location */
-int getMemLocation(int v){
-  return (v==BANKMODE ? 0 : BANKSIZE*curBank)+v;
+int getMemLocation(int v) {
+  return (v == BANKMODE ? 0 : BANKSIZE * curBank) + v;
 }
 
-void loadConfig(){
+void loadConfig() {
   // Read config values from EEPROM
   for (int i = 0; i < NUMCONFIG; i++) {
     config[i] = EEPROM.read(getMemLocation(i));
@@ -52,30 +52,30 @@ void loadConfig(){
   }
 }
 
-void saveConfig(int which=-1){
-  byte saved=0;
-  for(int v=0;v<NUMCONFIG;v++){
-    if(which==-1 || v==which){
+void saveConfig(int which = -1) {
+  byte saved = 0;
+  for (int v = 0; v < NUMCONFIG; v++) {
+    if (which == -1 || v == which) {
       // Save to EEPROM if the current configuration value has changed
       if (EEPROM.read(getMemLocation(v)) != config[v]) {
-         ph("Writing to eeprom");
-         p("v",v);
-         pln("conf",config[v]);
-         EEPROM.write(getMemLocation(v), config[v]);
-         saved++;
+        ph("Writing to eeprom");
+        p("v", v);
+        pln("conf", config[v]);
+        EEPROM.write(getMemLocation(v), config[v]);
+        saved++;
       }
     }
   }
   ///////////////012345678901234567890
-  if(saved>1){
-    lcd.printRow(3,"Saved changes...");
+  if (saved > 1) {
+    lcd.printRow(3, "Saved changes...");
     delay(250);
-  }else if(saved>0){
+  } else if (saved > 0) {
     ///////////////012345678901234567890
-    lcd.printRow(3,"Saved change.");
+    lcd.printRow(3, "Saved change.");
     delay(250);
   }
-  
+
 }
 
 
@@ -86,11 +86,11 @@ void setupButtons() {
   pinMode(BUTTON_MINUS, INPUT_PULLUP);
   delay(1);
   loadConfig();
-  byte r=readButtons(); //returns 0 to 3
-  if(r){
-    curBank=r-1; //sets 1 to 2
-  }else{
-    curBank=config[BANKMODE];
+  byte r = readButtons(); //returns 0 to 3
+  if (r) {
+    curBank = r - 1; //sets 1 to 2
+  } else {
+    curBank = config[BANKMODE];
   }
   loadConfig(); //reload due to bankmode change
 }
@@ -109,117 +109,118 @@ bool minusButton() {
 
 
 
-void displayConfig(bool header=true) {
+void displayConfig(bool header = true) {
   setDevice(0);
   ///////////CONFIGURATION
   ///////////01234567890123456789
   char label[22];
-  if(header) {
-    snprintf(label,sizeof(label),"Config %s mode:",BANKNAMES[curBank]);
-    lcd.printRow(2,label);
+  if (header) {
+    snprintf(label, sizeof(label), "Config %s mode:", BANKNAMES[curBank]);
+    lcd.printRow(2, label);
   }
-  strncpy_P(label, VLABELS[v], sizeof(label) - 1); 
+  strncpy_P(label, VLABELS[v], sizeof(label) - 1);
   label[20] = '\0'; //null term
   lcd.setCursor(0, 3);
   lcd.print(label);
   if (v != 0) {
     lcd.setCursor(17, 3);
-    if(v==BANKMODE){
+    if (v == BANKMODE) {
       lcd.setCursor(15, 3);
       lcd.print(BANKNAMES[config[v]]);
-    }else if(v==MINLAPDUR || v==YELLOWDELTA ){
+    } else if (v == MINLAPDUR || v == YELLOWDELTA ) {
       lcd.setCursor(15, 3);
       lcd.print(config[v]*MINLAPDURSTEP);
-    }else if(v==FUELING){ //Not a time thing but larger number
+    } else if (v == FUELING) { //Not a time thing but larger number
       lcd.setCursor(15, 3);
-      lcd.print(config[v]*MINLAPDURSTEP);  
-    }else if(v==REDLAPSNUMER){
+      lcd.print(config[v]*MINLAPDURSTEP);
+    } else if (v == REDLAPSNUMER) {
       lcd.setCursor(15, 3);
-      lcd.print((double)config[v]/FRACTIONDENOM);
-    }else if(VMAX[v]==1){
-      lcd.print(config[v]?"On":"Off");
-    }else if(VMAX[v]==0){
+      lcd.print(config[v]);
+      lcd.print("%");
+    } else if (VMAX[v] == 1) {
+      lcd.print(config[v] ? "On" : "Off");
+    } else if (VMAX[v] == 0) {
       //nop
-    }else lcd.print(config[v]);
+    } else lcd.print(config[v]);
   }
   ph("displayConfig");
-  p("label",label);
-  pln("cval",(int)config[v]);
+  p("label", label);
+  pln("cval", (int)config[v]);
 }
 
-void displayAllConfig(){
+void displayAllConfig() {
   char buf[40];
-             //01234567890123456789
-  snprintf(buf,sizeof(buf),"%s Mode",BANKNAMES[curBank]);
-  lcd.printRow(0,buf);
-  for(v=0;v<NUMCONFIG;v++){
+  //01234567890123456789
+  snprintf(buf, sizeof(buf), "%s Mode", BANKNAMES[curBank]);
+  lcd.printRow(0, buf);
+  for (v = 0; v < NUMCONFIG; v++) {
     displayConfig(false);
     delay(200);
   }
-  v=0;
+  v = 0;
 }
 
 
 
 
-void seeChange(bool inc){
-   if(v==DEFAULTS){
-    if(inc){
-      lcd.printRow(0,"Set to defaults...");
+void seeChange(bool inc) {
+  if (v == DEFAULTS) {
+    if (inc) {
+      lcd.printRow(0, "Set to defaults...");
       delay(50);
-       // Read config values from EEPROM
+      // Read config values from EEPROM
       for (int i = 0; i < NUMCONFIG; i++) {
-          config[i] = VDEF[curBank][i]; // Set to default value 
-      }  
-      displayAllConfig();
-      v=DEFAULTS;
-    }
-   }else  if(v==BRIGHTNESS){
-      setColor(YELLOW);
-   }else if(v==RESUME){
-     if(inc) {
-      saveConfig();
-     }else{
-      lcd.printRow(3,"Temporary changes...");
-     }
-     delay(300);
-   }else if(v==SOUND){
-     playToneBlocking(200,100);
-   }else if(v==DEMODIAG && inc){    
-      lcd.printRow(3,"Reading sensors.");
-      delay(50);
-      for(int i=0;i<NUMSENSORS;i++){
-         lcd.setCursor(0,3);
-         sensors[i].display(i);
-         delay(300);
+        config[i] = VDEF[curBank][i]; // Set to default value
       }
-      lcd.printRow(3,"Lights demo... ");
-      lights.demo();
-      delay(1000);
-      lcd.printRow(3,"Start song...  ");
-      pln("Start song","");
-      playF1StartSound1();
-      delay(1000);
-      lcd.printRow(3,"Restart song...");
-      pln("Restart song","");
-      playF1Restart();
-      delay(1000);
-      lcd.printRow(3,"Red flag song...    ");
-      pln("Red flag song","");
-      playMusic(imperialMarchMelody,imperialMarchNotes,120);
-      delay(1000);
-      lcd.printRow(3,"Win song...");
-      pln("Win song","");
-      playMusic(odeToJoyMelody,odeToJoyNotes,80*4);  
-   }
+      displayAllConfig();
+      v = DEFAULTS;
+    }
+  } else  if (v == BRIGHTNESS) {
+    setColor(YELLOW);
+  } else if (v == RESUME) {
+    if (inc) {
+      saveConfig();
+    } else {
+      lcd.printRow(3, "Temporary changes...");
+    }
+    delay(300);
+  } else if (v == SOUND) {
+    playToneBlocking(200, 100);
+  } else if (v == DEMODIAG && inc) {
+    lcd.printRow(3, "Reading sensors.");
+    delay(50);
+    for (int i = 0; i < NUMSENSORS; i++) {
+      lcd.setCursor(0, 3);
+      sensors[i].display(i);
+      delay(300);
+    }
+    lcd.printRow(3, "Lights demo... ");
+    lights.demo();
+    delay(1000);
+    lcd.printRow(3, "Start song...  ");
+    pln("Start song", "");
+    playF1StartSound1();
+    delay(1000);
+    lcd.printRow(3, "Restart song...");
+    pln("Restart song", "");
+    playF1Restart();
+    delay(1000);
+    lcd.printRow(3, "Red flag song...    ");
+    pln("Red flag song", "");
+    playMusic(imperialMarchMelody, imperialMarchNotes, 120);
+    delay(1000);
+    lcd.printRow(3, "Win song...");
+    pln("Win song", "");
+    playMusic(odeToJoyMelody, odeToJoyNotes, 80 * 4);
+  }
 }
 
 
-bool selectPressed=false;
+bool selectPressed = false;
 
 bool configByButtons() {
   if (selectButton()) {
-    if(v==BANKMODE){
+    if (v == BANKMODE) {
       saveConfig(v);
     }
     v++; // Increment the selection index
@@ -229,7 +230,7 @@ bool configByButtons() {
     delay(300); // Debounce delay
     if (v == 0) requireUpToStart = true;
     else requireUpToStart = false;
-    selectPressed=true;
+    selectPressed = true;
   } else if (plusButton() && selectPressed) {
     requireUpToStart = false;
     if (++config[v] > VMAX[v]) config[v] = VMAX[v];
@@ -238,22 +239,22 @@ bool configByButtons() {
     displayConfig(); // Update display with new value
   } else if (minusButton() && selectPressed) {
     requireUpToStart = false;
-    if(config[v]>0) config[v]--;
+    if (config[v] > 0) config[v]--;
     if (config[v] < 0) config[v] = 0;
     seeChange(false);
     delay(100); // Debounce delay
     displayConfig(); // Update display with new value
   }
 
-//  // For debugging purposes, print the current variable and its value
-//  if(serialOn) Serial.print("Config[");
-//  if(serialOn) Serial.print(v);
-//  if(serialOn) Serial.print("] = ");
-//  if(serialOn) Serial.println(config[v]);
+  //  // For debugging purposes, print the current variable and its value
+  //  if(serialOn) Serial.print("Config[");
+  //  if(serialOn) Serial.print(v);
+  //  if(serialOn) Serial.print("] = ");
+  //  if(serialOn) Serial.println(config[v]);
 
   delay(10); // Loop delay to prevent bouncing issues
   if (v != 0 || requireUpToStart) return true; // Inhibit rest of loop, must select back to config 0 to resume
-  selectPressed=false;
+  selectPressed = false;
   return false;
 }
 
